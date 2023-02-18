6a530c6c5fd0ed0a3474ec60c3061f4b89280d49
bukk-funds <67737845+bukk-funds@users.noreply.github.com>
2023-02-18 16:08:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 
+ menu = document.querySelector(".menu");
+    menu.onclick = function () {
+    nav = document.querySelector(".nav");
+    nav.classList.toggle("active");
+};
+
+function showDropSearch() {
+    let dropDiv = document.querySelector(".dropdownSearch");
+
+    if (dropDiv.style.display === "block") {
+        dropDiv.style.display = "";
+    } 
+    else {
+        dropDiv.style.display = "block";
+    }
+}
